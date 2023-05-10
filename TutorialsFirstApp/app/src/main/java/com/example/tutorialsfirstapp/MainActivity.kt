1cde9fdd0715f885d2080c8276e217e5887724d0
Veselin Velkov <vesselin60@gmail.com>
2023-05-10 17:52:44 +0300

    TutorialsFirstApp Part1

--- TutorialsFirstApp/app/src/main/java/com/example/tutorialsfirstapp/MainActivity.kt
+++ TutorialsFirstApp/app/src/main/java/com/example/tutorialsfirstapp/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.tutorialsfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            Log.i("Gocho", "Buton Pulsado ${etName.text.toString()}")
+        }
 //        Al arrancar la pantalla
     }
 }
