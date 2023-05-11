04049e7ba05bf3d4f27203670f49f1cb8b452f87
Veselin Velkov <vesselin60@gmail.com>
2023-05-11 15:42:42 +0300

    Desarrollando nuestra primera APP Part2

--- TutorialsFirstApp/app/src/main/java/com/example/tutorialsfirstapp/MainActivity.kt
+++ TutorialsFirstApp/app/src/main/java/com/example/tutorialsfirstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tutorialsfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,7 +16,15 @@ class MainActivity : AppCompatActivity() {
         val etName = findViewById<AppCompatEditText>(R.id.etName)
 
         btnStart.setOnClickListener {
-            Log.i("Gocho", "Buton Pulsado ${etName.text.toString()}")
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+
+
         }
 //        Al arrancar la pantalla
     }
