a3fc92b187dca367d8e4aefd1870e1279750e0d8
Veselin Velkov <vesselin60@gmail.com>
2023-05-28 16:12:41 +0300

    Tutorials Third App: finish creation of the categories list

--- TodoListApp/app/src/main/java/com/example/todolistapp/CategoriesViewHolder.kt
+++ TodoListApp/app/src/main/java/com/example/todolistapp/CategoriesViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.todolistapp
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -10,6 +11,26 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val divider: View = view.findViewById(R.id.divider)
 
     fun render(taskCategory: TaskCategory) {
-        tvCategoryName.text = "TEXT HOLDER"
+
+        when(taskCategory) {
+            TaskCategory.Business -> {
+                tvCategoryName.text = "Business"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_business_category)
+                )
+            }
+            TaskCategory.Other -> {
+                tvCategoryName.text = "Other"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_other_category)
+                )
+            }
+            TaskCategory.Personal -> {
+                tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_personal_category)
+                )
+            }
+        }
     }
 }
