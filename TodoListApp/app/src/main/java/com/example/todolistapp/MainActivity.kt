c310c54691e0372eab52f592e4c8f81e9a632fdd
Veselin Velkov <vesselin60@gmail.com>
2023-05-29 17:24:07 +0300

    Radio Button added, button tint not working

--- TodoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ TodoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -1,34 +1,55 @@
 package com.example.todolistapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todolistapp.TaskCategory.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private val categories = listOf(
-        TaskCategory.Business,
-        TaskCategory.Personal,
-        TaskCategory.Other
+        Business,
+        Personal,
+        Other
+    )
+
+    private val tasks = mutableListOf(
+        Task("BusinessTextHolder", Business),
+        Task("PersonalTextHolder", Personal),
+        Task("OtherTextHolder", Other)
     )
 
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
 
     private lateinit var rvTasks: RecyclerView
-//    private lateinit var tasksAdapter: TasksAdapter
+    private lateinit var tasksAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initComponents()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
     }
 
     private fun initComponents() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
@@ -36,5 +57,9 @@ class MainActivity : AppCompatActivity() {
         rvCategories.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
     }
 }
--- TodoListApp/app/src/main/java/com/example/todolistapp/TasksAdapter.kt
+++ TodoListApp/app/src/main/java/com/example/todolistapp/TasksAdapter.kt
@@ -1,16 +1,19 @@
 package com.example.todolistapp
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TasksAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TaskViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        
+class TasksAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TasksViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        return TasksViewHolder(view)
     }
 
     override fun getItemCount() = tasks.size
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        holder.render(tasks[position])
     }
 }
