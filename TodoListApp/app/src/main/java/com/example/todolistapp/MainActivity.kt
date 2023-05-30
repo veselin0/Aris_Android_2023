34df250c20bf499a3eba90cba8e9e2145fd672ed
Veselin Velkov <vesselin60@gmail.com>
2023-05-30 11:13:40 +0300

    Radio Button OK, add dialog prompt

--- TodoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ TodoListApp/app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.todolistapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolistapp.TaskCategory.*
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var fabAddTask: FloatingActionButton
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,12 +43,40 @@ class MainActivity : AppCompatActivity() {
         initListeners()
     }
 
+    private fun updateTasks() {
+        tasksAdapter.notifyDataSetChanged()
+    }
+
     private fun initListeners() {
         fabAddTask.setOnClickListener { showDialog() }
     }
 
     private fun showDialog() {
         val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            val currentTask = etTask.text.toString()
+            if (currentTask.isNotEmpty()) {
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val currentCategory: TaskCategory = when (selectedRadioButton.text) {
+                    getString(R.string.business) -> Business
+                    getString(R.string.personal) -> Personal
+                    else -> Other
+                }
+
+                tasks.add(Task(currentTask, currentCategory))
+                updateTasks()
+                dialog.hide()
+            }
+        }
+
+        dialog.show()
     }
 
     private fun initComponents() {
